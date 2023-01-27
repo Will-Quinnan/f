a2d7eb1c99b7312058101d40d704706a1a755b0b
Will <wmplqnjr@gmail.com>
2023-01-26 19:02:51 -0800

    pushing to git

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { React } from 'react';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
 
       <h1>Hello from React</h1>
-
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            Will Q
+        </div>
+    );
+}
+
+export default Footer;
