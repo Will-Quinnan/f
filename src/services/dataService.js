a2aa6dae6ef88641e5cb465e9d985fa0f43ea732
Will <wmplqnjr@gmail.com>
2023-01-31 17:25:31 -0800

    more typos

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,45 +1,65 @@
 var catalog = [
     {
-        "title":"Salmon",
-        "category": "fish",
-        "image": "salmon.png",
-        "_id":"0000001"
+        title:"Salmon",
+        category: "fish",
+        image: "salmon.png",
+        _id:"0000001",
+        price: 12.00
     },
 
     {
-        "title":"Cod",
-        "category": "fish",
-        "image": "images/cod.jpg",
-        "_id":"0000002"
+        title:"Cod",
+        category: "fish",
+        image: "images/cod.jpg",
+        _id:"0000002",
+        price: 12.00
     },
     {
-        "title":"Iceland Haddock",
-        "category": "fish",
-        "image": "images/iceland haddock.jpg",
-        "_id":"0000003"
+        title:"Iceland Haddock",
+        category: "fish",
+        image: "images/iceland haddock.jpg",
+        _id:"0000003",
+        price: 12.00
     },
     {
-        "title":"Mackrel",
-        "category": "fish",
-        "image": "images/mackrel.jpg",
-        "_id":"0000004"
+        title:"Mackrel",
+        category: "fish",
+        image: "images/mackrel.jpg",
+        _id:"0000004",
+        price: 12.00
     },
     {
-        "title":"Seabass",
-        "category": "fish",
-        "image": "images/seabass.jpg",
-        "_id":"0000005"
+        title:"Seabass",
+        category: "fish",
+        image: "images/seabass.jpg",
+        _id:"0000005",
+        price: 12.00
     },
     {
-        "title":"Tilapia",
-        "category": "fish",
-        "image": "images/tilapia.jpg",
-        "_id":"0000006"
+        title:"Tilapia",
+        category: "fish",
+        image: "images/tilapia.jpg",
+        _id:"0000006",
+        price: 12.00
     },
     {
-        "title":"Tuna",
-        "category": "fish",
-        "image": "images/tuna.jpg",
-        "_id":"0000007"
-    },
+        title:"Tuna",
+        category: "fish",
+        image: "images/tuna.jpg",
+        _id:"0000007",
+        price: 12.00
+    }
 ];
+
+class DataService {
+
+    getProducts(){
+        //To Do: connect to server and retrieve to display
+
+        return catalog:
+    }
+
+}
+
+
+export default DataService;
