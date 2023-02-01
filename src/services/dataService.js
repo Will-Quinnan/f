5a7ed4015d0d6db54117a6484b0074187aa3cab9
Will <wmplqnjr@gmail.com>
2023-01-31 17:18:30 -0800

    typos

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,8 +1,45 @@
 var catalog = [
     {
-        "title":"Fish",
+        "title":"Salmon",
         "category": "fish",
-        "image": "images/salmon.png",
-        "_id":"asadf1234"
+        "image": "salmon.png",
+        "_id":"0000001"
+    },
+
+    {
+        "title":"Cod",
+        "category": "fish",
+        "image": "images/cod.jpg",
+        "_id":"0000002"
+    },
+    {
+        "title":"Iceland Haddock",
+        "category": "fish",
+        "image": "images/iceland haddock.jpg",
+        "_id":"0000003"
+    },
+    {
+        "title":"Mackrel",
+        "category": "fish",
+        "image": "images/mackrel.jpg",
+        "_id":"0000004"
+    },
+    {
+        "title":"Seabass",
+        "category": "fish",
+        "image": "images/seabass.jpg",
+        "_id":"0000005"
+    },
+    {
+        "title":"Tilapia",
+        "category": "fish",
+        "image": "images/tilapia.jpg",
+        "_id":"0000006"
+    },
+    {
+        "title":"Tuna",
+        "category": "fish",
+        "image": "images/tuna.jpg",
+        "_id":"0000007"
     },
 ];
